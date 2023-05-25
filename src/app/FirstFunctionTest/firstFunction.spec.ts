e00d48a86acc400cbd9580f52f9a9002a27a0702
Paulo Rodrigues <paulinho.junio.reis@gmail.com>
2023-05-25 15:31:16 -0300

    Removing all examples from master

--- src/app/FirstFunctionTest/firstFunction.spec.ts
+++
@@ -1,7 +0,0 @@
-import { addition } from "./firstFunction";
-
-describe('Test addition', () =>{
-    it('Testing addition function', () => {
-        expect(addition(10,20)).toBe(30); //todos os testes devem termina com expect ( obvio, porem e' bom lembra!!! )
-    })
-});
